0dbdc9587c2e9992c845444299804bcfbd1e3d74
Tom Freudenheim <tomfreudenheim@me.com>
2017-08-29 13:10:22 -0400

    Add tests for logged in and logged out users

--- example_app/test/controllers/page_controller_test.exs
+++ example_app/test/controllers/page_controller_test.exs
@@ -1,8 +1,19 @@
 defmodule ExampleApp.PageControllerTest do
   use ExampleApp.ConnCase
+  alias ExampleApp.User
 
-  test "GET /", %{conn: conn} do
+  test "logged out GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Page available for logged in users only"
+  end
+
+  test "logged in GET /", %{conn: conn} do
+    user = User.changeset(%User{}, %{name: "John",
+                                     email: "john@example.com",
+                                     encrypted_password: "encryped"})
+           |> Repo.insert!
+    Plug.Test.init_test_session(conn, %{current_user: user})
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "don't worry about user management"
   end
 end
